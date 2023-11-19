dc0afd297bcf237f855c628cd23f04e5a8078910
eprush <pavlovich.egorka@list.ru>
2023-11-20 00:05:38 +0300

    do something

--- Strings/z_function.py
+++ Strings/z_function.py
@@ -10,12 +10,17 @@ def z_func_triv(s: str) -> list:
 def z_func(s: str) -> list:
     length = len(s)
     z = [0] * length
-    left, right = 0, 0
+    boundaries = [0, 0]
+
+    def update(pos: int):
+        if pos + z[pos] - 1 > boundaries[1]:
+            boundaries[0], boundaries[1] = pos, pos + z[pos] - 1
+        return
+
     for i in range(1, length):
-        if i <= right:
-            z[i] = min(right - i + 1, z[i - left])
+        if i <= boundaries[1]:
+            z[i] = min(boundaries[1] - i + 1, z[i - boundaries[0]])
         while i + z[i] < length and s[z[i]] == s[i + z[i]]:
             z[i] += 1
-        if i + z[i] - 1 > right:
-            left, right = i, i + z[i] - 1
+        update(i)
     return z
